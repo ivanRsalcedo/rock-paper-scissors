1085a44a3b4c8576946ccaaf503ada7f03850745
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-07-08 15:19:21 -0400

    Convert console-based game to UI-based game

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
+
 let humanScore = 0, computerScore = 0;
+let gameOver = false;
+const resultsDisplay = document.querySelector('#results-display');
 
-playGame();
-
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
+document.querySelector('#choice-buttons').addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON' && !gameOver) {
+        playRound(e.target.textContent, getComputerChoice());
     }
-    console.log(humanScore === computerScore ? "After all, it's a tie." :
-        humanScore > computerScore ? "Human wins the game!" :
-        "Computer wins the game!");
-    console.log(`Scores: ${humanScore}:${computerScore}`);
-}
+});
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice)
-        console.log(`It's a tie!`);
+        resultsDisplay.textContent = `TIE!`;
     else if (
-        (humanChoice === "rock" && computerChoice === "paper") ||
-        (humanChoice === "paper" && computerChoice === "scissors") ||
-        (humanChoice === "scissors" && computerChoice === "rock")
+        (humanChoice === "✊" && computerChoice === "🖐") ||
+        (humanChoice === "🖐" && computerChoice === "✌") ||
+        (humanChoice === "✌" && computerChoice === "✊")
     ) {
-        console.log(`You lose! \n${computerChoice} beats ${humanChoice}.\n Human Score: ${humanScore}\n Computer Score: ${++computerScore}`);
+        showResult(`Computer wins! \n${computerChoice} beats ${humanChoice}`);
+        document.getElementById('computer-score').textContent = ++computerScore;
+    } else {
+        showResult(`Human wins! \n${humanChoice} beats ${computerChoice}`);
+        document.getElementById('human-score').textContent = ++humanScore;
     }
-
-    else
-        console.log(`You win! \n${humanChoice} beats ${computerChoice}.\n Human Score: ${++humanScore}\n Computer Score: ${computerScore}`);
+    if (humanScore === 5 || computerScore === 5) {
+        showResult(`Game Over!\n${humanScore === 5 ? "Human" : "Computer"} wins!`);
+        gameOver = true;
+    }
 }
 
 function getComputerChoice() {
-    return ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
+    return ["✊", "🖐", "✌"][Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice() {
-    return prompt("Rock, paper, or scissors?").toLowerCase();
+function showResult(string) {
+    resultsDisplay.textContent = string;
 }
